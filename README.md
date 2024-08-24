1e5119584522abb46955c32c9c3438a5a064a019
Canop <cano.petrole@gmail.com>
2024-08-24 16:49:24 +0200

    program grew a little with the kids

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
-A very simple program asking questions like `35 + 12 = ?` in a loop.
+A very simple program asking questions like `35 + 12 = ?` in a loop until a predefined goal is reached.
 
 The code is easy to tune and change with the kid, it's a basis for a coding play too.
 
-It's *not* idiomatic Rust: the priority is the easy hackability under a kid's pressure.
-
 ![screenshot](screenshot.png)
+
+# Prerequesite
+
+* some basic programming and terminal know-how
+* git
+* rust: https://rustup.rs/
+
+# Install
+
+Clone this repository, go inside:
+
+```
+git clone https://github.com/Canop/kidding
+cd kidding
+```
+
+# Modify
+
+Open the `src/main.rs` file, try to see where the questions are defined, modify those parts.
+
+You can add or remove question kinds, and change the number ranges for each kind.
+
+I tried to make it obvious and easy to change but don't hesitate to come to my chat if you have any question: https://miaou.dystroy.org/3
+
+# Launch
+
+Just run `cargo run` : the program will be recompiled and executed.
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ use {
     },
 };
 
+#[allow(dead_code)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+enum Lang {
+    Fr,
+    En,
+}
+
+// This file is MEANT to be modified. Run it, change it for your kid, and run it again.
+
+const LANG: Lang = Lang::En; // <- change this to Fr to have the messages in French
+
 struct QA {
     question: String,
     answer: i64,
@@ -15,11 +26,15 @@ impl QA {
     fn new() -> Self {
         let mut rng = rand::thread_rng();
         match rng.gen_range(0, 7) {
+            // Generate a random number between 0 and 6
+            // you may comment out a case to remove it from the possible operations if you
+            // don't like it, you may also add more cases to add more operations (but then
+            // you should also change the range of the random number just above)
             0 => {
                 // halving
                 let a = rng.gen_range(4, 60);
                 Self {
-                    question: format!("{} / 2", a*2),
+                    question: format!("{} / 2", a * 2),
                     answer: a,
                 }
             }
@@ -33,7 +48,7 @@ impl QA {
             }
             2 => {
                 // multiplication
-                let (a, b) = (rng.gen_range(2, 9), rng.gen_range(2, 12));
+                let (a, b) = (rng.gen_range(2, 5), rng.gen_range(2, 6));
                 Self {
                     question: format!("{} x {}", a, b),
                     answer: a * b,
@@ -53,15 +68,14 @@ impl QA {
                         write!(&mut question, " - {}", a).unwrap();
                     }
                 }
-                Self {
-                    question,
-                    answer,
-                }
+                Self { question, answer }
             }
             4 => {
                 // addition of two big numbers
-                let a = rng.gen_range(20, 60) * rng.gen_range(25, 55) * rng.gen_range(2, 15);
-                let b = rng.gen_range(25, 50) * rng.gen_range(15, 65) * rng.gen_range(2, 10);
+                let a = rng.gen_range(20, 60) * rng.gen_range(10, 75) * rng.gen_range(2, 15)
+                    + rng.gen_range(1, 100);
+                let b = rng.gen_range(25, 45) * rng.gen_range(15, 60) * rng.gen_range(2, 10)
+                    + rng.gen_range(0, 9000);
                 Self {
                     question: format!("{} + {}", a, b),
                     answer: a + b,
@@ -81,24 +95,83 @@ impl QA {
 
 fn main() {
     let stdin = stdin();
-    let mut input = stdin.lock().lines().flatten();
+    let mut input = stdin.lock().lines().map_while(Result::ok);
     let (mut good, mut bad) = (0, 0);
+    let mut successive_good = 0;
+    let min_successive_good = 3; // <- change this to set the number of good answers in a row to win
+    let min_good = 10; // <- change this to set the number of good answers to win
+    let goal_percent = 80; // <- change this to set the percentage of good answers to win
+    let mut qa = QA::new();
+    match LANG {
+        Lang::Fr => {
+            println!(
+                "Pour gagner, il faut avoir {} bonnes réponses de suite, {} bonnes réponses au total, ET au moins {}% de réussite",
+                min_successive_good,
+                min_good,
+                goal_percent,
+            );
+        }
+        Lang::En => {
+            println!(
+                "To win, you need {} successive right answers, a total of {} right answers, and a win rate at least {}%",
+                min_successive_good,
+                min_good,
+                goal_percent,
+            );
+        }
+    }
     loop {
-        let qa = QA::new();
         println!("{} = ?", qa.question);
-        match input.next().unwrap().parse::<i64>() {
+        match input.next().unwrap().trim().parse::<i64>() {
             Ok(answer) => {
                 if answer == qa.answer {
-                    println!("{}", "Right!".green());
+                    match LANG {
+                        Lang::Fr => println!("{}", "Juste".green()),
+                        Lang::En => println!("{}", "Right".green()),
+                    }
                     good += 1;
+                    successive_good += 1;
+                    if good >= min_good {
+                        if successive_good >= min_successive_good {
+                            let percent = 100 * good / (good + bad);
+                            let won = percent >= goal_percent;
+                            println!("{} réussites sur {} : {}%", good, good + bad, percent);
+                            match LANG {
+                                Lang::Fr => {
+                                    println!(
+                                        "{} réussites sur {} : {}%",
+                                        good,
+                                        good + bad,
+                                        percent
+                                    );
+                                }
+                                Lang::En => {
+                                    println!("{} wins over {} : {}%", good, good + bad, percent);
+                                }
+                            }
+                            if won {
+                                println!("BRAVO!");
+                                break;
+                            }
+                        }
+                    } else {
+                        match LANG {
+                            Lang::Fr => println!("{} bonnes réponses sur {}", good, min_good),
+                            Lang::En => println!("{} right answers over {}", good, min_good),
+                        }
+                    }
                 } else {
-                    println!("{} {} = {}", "Wrong!".red(), qa.question, qa.answer);
+                    match LANG {
+                        Lang::Fr => println!("{} {} = {}", "Faux!".red(), qa.question, qa.answer),
+                        Lang::En => println!("{} {} = {}", "Wrong!".red(), qa.question, qa.answer),
+                    }
                     bad += 1;
+                    successive_good = 0;
                 }
+                qa = QA::new();
             }
-            Err(_) => { // anything not a number quits
-                println!("You got {} / {}", good, good + bad );
-                break;
+            Err(_) => { // anything not a number quits if you uncomment the following line
+                 //break;
             }
         }
     }
